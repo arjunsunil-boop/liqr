369d155da018084858cef9d5ed61235526f0fe81
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2025-11-09 16:02:33 +0530

    folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import Home from './components/Home'
+import Home from './components/Home/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import DelMod from './components/DelMod'
-import AddLiq from './components/AddLiq'
-import Login from './components/Login'
+import DelMod from './components/DelMod/DelMod'
+import AddLiq from './components/AddLiq/AddLiq'
+import Login from './components/Login/Login'
 import Signup from './components/Signup'
-import ViewLiq from './components/ViewLiq'
+import ViewLiq from './components/ViewLiq/ViewLiq'
 
 
 
